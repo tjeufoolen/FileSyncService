7a708f0c0351e214deca14cd1d814e52251a4d85
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2020-12-17 19:56:18 +0100

    Added ascii arts!

--- client/main.cpp
+++ client/main.cpp
@@ -2,6 +2,20 @@
 #include "src/Client.h"
 #include "src/utils/Logger.h"
 
+/*
+ * File management with
+ * ,------.                       ,--.
+ * |  .-.  \ ,--.--. ,---.  ,---. |  |-.  ,---.,--.  ,--.
+ * |  |  \  :|  .--'| .-. || .-. || .-. '| .-. |\  `'  /
+ * |  '--'  /|  |   ' '-' '| '-' '| `-' |' '-' '/  /.  \
+ * `-------' `--'    `---' |  |-'  `---'  `---''--'  '--'
+ *                         `--'
+ *
+ * Made by Tjeu Foolen
+ * Commissioned by Bob Polis (Teacher Avans Hogeschool at Den Bosch)
+ * 2 January 2021)
+ */
+
 int main() {
     try {
         std::unique_ptr<Client> c = std::make_unique<Client>("localhost", "12345");
--- server/main.cpp
+++ server/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include "src/Server.h"
 
+/*
+ * File management with
+ * ,------.                       ,--.
+ * |  .-.  \ ,--.--. ,---.  ,---. |  |-.  ,---.,--.  ,--.
+ * |  |  \  :|  .--'| .-. || .-. || .-. '| .-. |\  `'  /
+ * |  '--'  /|  |   ' '-' '| '-' '| `-' |' '-' '/  /.  \
+ * `-------' `--'    `---' |  |-'  `---'  `---''--'  '--'
+ *                         `--'
+ *
+ * Made by Tjeu Foolen
+ * Commissioned by Bob Polis (Teacher Avans Hogeschool at Den Bosch)
+ * 2 January 2021)
+ */
+
 int main() {
     try {
         std::unique_ptr<Server> s = std::make_unique<Server>(12345);
